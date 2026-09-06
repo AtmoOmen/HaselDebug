e4e7ccf372de3f14cb18203f1ea0767fc3e24fec
Haselnussbomber <mail@haselnussbomber.de>
2026-09-06 15:10:14 +0200

    Add Online Store column to Outfits tab

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,7 @@
 ## [Unreleased]
 
 - **Added:** Type redirect for GuildleveAssignmentEventHandler.
+- **Added:** The Outfits unlock tab now has a Online Store column, displaying whether the set can be bought.
 - **Updated:** The plugin now disposes asynchronously, concurrently and on the Framework thread.
 - **Updated:** The GameWindow tab was removed because a special case for the Arguments pointer was added to draw them as array.
 - **Updated:** ClientStructs now at [d8633414](https://github.com/aers/FFXIVClientStructs/tree/d8633414) ([compare](https://github.com/aers/FFXIVClientStructs/compare/5af9ff4c..d8633414)).
---
+++ HaselDebug/Tabs/UnlocksTabs/Outfits/Columns/StoreItemColumn.cs
@@ -0,0 +1,49 @@
+using Dalamud.Utility;
+using HaselCommon.Gui.ImGuiTable;
+
+namespace HaselDebug.Tabs.UnlocksTabs.Outfits.Columns;
+
+[RegisterSingleton, AutoConstruct]
+public partial class StoreItemColumn : Column<MirageStoreSetItem>
+{
+    private readonly ITextureProvider _textureProvider;
+    private readonly TextService _textService;
+
+    public OutfitsTable Table;
+
+    [AutoPostConstruct]
+    public void Initialize()
+    {
+        SetFixedWidth(80);
+    }
+
+    public override int Compare(MirageStoreSetItem lhs, MirageStoreSetItem rhs)
+    {
+        return Table.IsStoreSet(lhs).CompareTo(Table.IsStoreSet(rhs));
+    }
+
+    public override void DrawColumn(MirageStoreSetItem row)
+    {
+        if (Table.IsStoreSet(row))
+        {
+            _textureProvider.DrawIcon(61831, OutfitsTable.IconSize);
+
+            var url = Table.GetStoreUrl(row.Items.First(item => item.RowId != 0 && item.IsValid));
+            var hasUrl = !string.IsNullOrEmpty(url);
+
+            if (ImGui.IsItemHovered())
+            {
+                using var tooltip = ImRaii.Tooltip();
+                ImGui.Text(_textService.Translate("HaselDebug.Tabs.UnlocksTabs.Outfits.Columns.StoreItemColumn.Tooltip"));
+
+                if (hasUrl)
+                    ImGui.SetMouseCursor(ImGuiMouseCursor.Hand);
+            }
+
+            if (hasUrl && ImGui.IsItemClicked())
+            {
+                Util.OpenLink(url);
+            }
+        }
+    }
+}
